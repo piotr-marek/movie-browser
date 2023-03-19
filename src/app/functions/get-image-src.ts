d636b55fc1bc03ef5b4c6e96377fbd294c731857
Piotr Marek <piotr.marek@nokia.com>
2023-03-19 23:37:21 +0100

    style adjustments for firefox, getImageSrc check for undefined, page title

--- src/app/functions/get-image-src.ts
+++ src/app/functions/get-image-src.ts
@@ -1,1 +1,1 @@
-export const getImageSrc = (url: string): string => url.match(/^http/) ? url : '../../assets/images/no-poster.svg'
+export const getImageSrc = (url: string): string => url?.match(/^http/) ? url : '../../assets/images/no-poster.svg'
