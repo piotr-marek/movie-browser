2b7979aa238efa2b2a5d3fc18c8b8d6a18dc11f7
Piotr Marek <piotr.marek@nokia.com>
2023-03-19 21:38:24 +0100

    errors, spinners, styles refactoring

--- src/app/components/recent/recent.component.ts
+++ src/app/components/recent/recent.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { MovieBasicInfo } from 'src/app/interfaces/movie-basic-info';
 import { RecentMovie } from 'src/app/interfaces/recent-movie';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-recent',
   templateUrl: './recent.component.html',
-  styleUrls: ['./recent.component.scss']
+  styleUrls: ['./recent.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecentComponent implements OnInit{
 
@@ -15,6 +17,8 @@ export class RecentComponent implements OnInit{
 
   ngOnInit(): void {
       this.recent$ = this._movieService.recent$;
-      //this.recent$.subscribe(v => console.log(JSON.parse(JSON.stringify(v)) ))
+  }
+  isValidRecentList(recent: Array<MovieBasicInfo>): boolean{
+    return recent.filter(movie => movie).length>1;
   }
 }
